8bfcd2d0dadb2440a3c5bbe45be8eab39277c21c
Marco Aurélio <marcoaurelio_db@hotmail.com>
2024-01-22 10:39:16 -0300

    bugfix: added a missing library

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,14 @@
 import express from "express"
-import { userRoutes } from "./users/user-routes" 
 import "./shared/translate/TranslationsYup"
+import { userRoutes } from "./users/user-routes" 
+import { adminRoutes } from "./admins/admin-routes"
+import { productRoutes } from "./products/product-routes"
 
 const app = express()
 
 app.use(express.json())
 app.use(userRoutes)
+app.use(adminRoutes)
+app.use(productRoutes)
 
 export { app }
